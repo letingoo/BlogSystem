b15681676ece0657063b32bda351bb63067419e6
letingoo <letingoo@hotmail.com>
2016-10-25 23:09:19 +0800

    实现博客发表和读取的功能

---
+++ src/main/java/blog/controller/BlogController.java
@@ -0,0 +1,66 @@
+package blog.controller;
+
+import blog.entity.Blog;
+import blog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import user.entity.User;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by letingoo on 2016/10/24.
+ */
+
+
+@Controller
+@RequestMapping("/blog")
+public class BlogController {
+
+
+    @Autowired
+    private BlogService service;
+
+
+    @RequestMapping("/toAddBlog")
+    public String toAddBlog() {
+        return "addBlog";
+    }
+
+
+    /**
+     * 增加Blog
+     * @param session
+     * @param blog
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/addBlog")
+    public String addBlog(HttpSession session, Blog blog) {
+
+        if ( session.getAttribute("user") == null )
+            return "fail";
+
+        blog.setUser( (User) session.getAttribute("user") );
+        service.addBlog(blog);
+
+        return "success";
+    }
+
+
+
+
+    public ModelAndView getBlogs(int pageNo, int pageSize) {
+
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject( "blogs", service.getBlogs(pageNo, pageSize) );
+        modelAndView.setViewName("blogs");
+
+        return modelAndView;
+    }
+
+
+}
---
+++ src/main/java/blog/entity/Blog.java
@@ -0,0 +1,52 @@
+package blog.entity;
+
+import user.entity.User;
+
+/**
+ * Created by letingoo on 2016/10/24
+ * 博客实体类.
+ */
+public class Blog {
+
+    private int id;
+
+    private String title;
+
+    // 内容，暂时用String类型
+    private String content;
+
+    private User user;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
