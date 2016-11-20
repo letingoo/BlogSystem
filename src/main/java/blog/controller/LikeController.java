8c32db474a6ba9656a6248cd4a0f6bfb80cec109
letingoo <letingoo@hotmail.com>
2016-11-20 14:23:35 +0800

    对cookie的操作，防止多次点赞

--- src/main/java/blog/controller/LikeController.java
+++ src/main/java/blog/controller/LikeController.java
@@ -2,10 +2,20 @@ package blog.controller;
 
 import blog.service.BlogService;
 import com.alibaba.druid.filter.AutoLoad;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by letingoo on 2016/11/10.
@@ -20,10 +30,30 @@ public class LikeController {
     private BlogService blogService;
 
 
+    private static Gson gson = new Gson();
+    private static final Type hashSetType =  new TypeToken<HashSet<Integer>>(){}.getType();
+
     @RequestMapping("/like/{blogId}")
-    public String likeBlog(@PathVariable("blogId") int blogId) {
+    @ResponseBody
+    public String likeBlog(@PathVariable("blogId") int blogId,
+                           @CookieValue(value = "likesSet",required = false)String likesSetStr,
+                           HttpServletResponse response) {
+
+
+        Set<Integer> likesSet = gson.fromJson(likesSetStr, hashSetType);
+
+        if (likesSet == null)
+            likesSet = new HashSet<Integer>();
+
+        if (likesSet.contains(blogId))
+            return "only once";
 
         blogService.likeBlog(blogId);
+
+        likesSet.add(blogId);
+        Cookie cookie = new Cookie("likesSet", likesSet.toString());
+        response.addCookie(cookie);
+
         return "success";
     }
 
